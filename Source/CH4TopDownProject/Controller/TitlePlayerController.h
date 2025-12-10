39cc0c2a72b60715160e3e74f80d00427286bdce
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-12-10 09:59:53 +0900

    [Add] TitleLayout, TitlePlayerController 클래스 및 BP 추가

---
+++ Source/CH4TopDownProject/Controller/TitlePlayerController.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TitlePlayerController.generated.h"
+
+UCLASS()
+class CH4TOPDOWNPROJECT_API ATitlePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+
+	void JoinServer(const FString& InIPAddress);
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = ASUIPlayerController, Meta = (AllowPrivateAccess))
+	TSubclassOf<UUserWidget> UIWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = ASUIPlayerController, Meta = (AllowPrivateAccess))
+	TObjectPtr<UUserWidget> UIWidgetInstance;
+};
