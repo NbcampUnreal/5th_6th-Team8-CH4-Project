1b24be7859f145a18a68fcf72cb1ddb0ca1b5e4f
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-12-11 16:09:40 +0900

    [Add] HealthComponent 클래스 원형 추가

---
+++ Source/CH4TopDownProject/Component/HealthComponent.cpp
@@ -0,0 +1,15 @@
+#include "Component/HealthComponent.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	
+
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
---
+++ Source/CH4TopDownProject/Component/HealthComponent.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CH4TOPDOWNPROJECT_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UHealthComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+
+		
+};
