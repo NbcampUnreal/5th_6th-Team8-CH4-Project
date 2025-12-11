6683272afb6f4471cb7ce757d1b20278ad90a070
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-12-11 15:50:05 +0900

    [Add] MainHUDWidget 클래스 추가

---
+++ Source/CH4TopDownProject/UI/MainHUDWidget.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainHUDWidget.generated.h"
+
+class UProgressBar;
+class UTextBlock;
+class UHorizontalBox;
+class UImage;
+
+UCLASS()
+class CH4TOPDOWNPROJECT_API UMainHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+    UFUNCTION(BlueprintCallable, Category = "HUD")
+    void UpdateHealthAndStamina(float CurrentHealth, float MaxHealth, float CurrentStamina, float MaxStamina);
+
+    UFUNCTION(BlueprintCallable, Category = "HUD")
+    void UpdateAmmoCount(int32 CurrentAmmo, int32 MaxClipAmmo);
+
+protected:
+    virtual void NativeConstruct() override;
+
+private:
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UProgressBar> HealthBar;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UProgressBar> StaminaBar;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UTextBlock> CurrentAmmoText;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UTextBlock> MaxAmmoText;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UHorizontalBox> QuickslotBox;
+};
