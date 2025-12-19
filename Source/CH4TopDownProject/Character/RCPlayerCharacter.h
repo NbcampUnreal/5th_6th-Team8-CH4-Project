e04aaa3c58d6adabab524c56411ca26af12e6fb4
Wonwoo126 <lom2468@naver.com>
2025-12-19 19:43:34 +0900

    Revert "[Mod] 캐릭터 걷기, 뛰기 사운드"
    This reverts commit 605f07e0d29d288bf3f5df45d8ca777b43b6c44d.

--- Source/CH4TopDownProject/Character/RCPlayerCharacter.h
+++ Source/CH4TopDownProject/Character/RCPlayerCharacter.h
@@ -211,27 +211,5 @@ protected:
 	UFUNCTION()
 	void OnRep_CurrentArmor();
 #pragma endregion
-
-#pragma region SFX
-protected:
-	UPROPERTY(EditDefaultsOnly, Category = "SFX|Walk")
-	TObjectPtr<USoundBase> FootstepSound;
-
-	float FootstepInterval = 0.35f;
-
-	UPROPERTY(EditDefaultsOnly, Category = "SFX|Dash")
-	TObjectPtr<USoundBase> DashSound;
-
-	UFUNCTION(NetMulticast, Unreliable)
-	void Multicast_PlayDashSFX(const FVector& Loc);
-
-private:
-	FTimerHandle FootstepTimerHandle;
-
-	void StartFootstepLoop();
-	void StopFootstepLoop();
-	void PlayFootstepOnce();
-
-#pragma endregion
 };
 
