8ccb575c2f1bd48ee8e4c745a766428717f98743
ko gyeong tae <rudex1598@gmail.com>
2025-12-21 18:03:01 +0900

    temp

--- Source/CH4TopDownProject/Inventory/InventoryComponent.cpp
+++ Source/CH4TopDownProject/Inventory/InventoryComponent.cpp
@@ -159,6 +159,7 @@ void UInventoryComponent::AddItem(FInventorySlot Item)
 	if (Item.ItemType == EItemType::Weapon) {
 		
 	}
+
 	UDataTable* itemdatatable = GetDataTableByItemType(Item.ItemType);
 	const FItemData* ItemRow = itemdatatable->FindRow<FItemData>(Item.ItemID, TEXT(""));
 	if (!ItemRow) {
