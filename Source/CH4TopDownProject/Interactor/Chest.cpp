757eb6c6850969975c429f19b9a139fa4af634cd
hortus0915 <86999699+hortus0915@users.noreply.github.com>
2025-12-24 20:01:45 +0900

    [Add] 상자 아이템 처리, 아이템 스폰 테이블(무기) 쪽 처리 수정

--- Source/CH4TopDownProject/Interactor/Chest.cpp
+++ Source/CH4TopDownProject/Interactor/Chest.cpp
@@ -3,9 +3,12 @@
 
 #include "Interactor/Chest.h"
 
+#include "Character/RCPlayerCharacter.h"
+#include "Net/UnrealNetwork.h"
+
 AChest::AChest()
 {
-	ItemList.Add(TEXT("Test"), 1);
+	//ItemList.Add(TEXT("Test"), 1);
 }
 	
 
@@ -18,11 +21,43 @@ void AChest::Interact_Implementation(AActor* Interactor)
 	// 이런식으로 호출하면 됨
 	// Player->OpenChestUI(this);
 
-	
+	if (!HasAuthority()) return; 
+
+	SetOwner(Interactor);
+	Client_OpenChestUI(Interactor);
+}
+
+void AChest::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AChest, ItemListArray);
 }
 
 void AChest::SetItem(const TMap<FName, int32>& ItemMap)
 {
-	ItemList = ItemMap;
-	
+	if (!HasAuthority()) return;
+
+	ItemListArray.Empty(); 
+	for (const auto& Pair : ItemMap)
+	{
+		ItemListArray.Add({Pair.Key, Pair.Value});
+	}
 }
+
+void AChest::Client_OpenChestUI_Implementation(AActor* Interactor)
+{
+	if (ItemListArray.Num() == 0)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Cyan, 
+				TEXT("EMPTY"));	
+	}
+	
+	for (const FChestItemEntry& Entry : ItemListArray)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Cyan, 
+			FString::Printf(TEXT("%s : %d"), *Entry.ItemName.ToString(), Entry.ItemNum));
+	}
+}
+
+
+
