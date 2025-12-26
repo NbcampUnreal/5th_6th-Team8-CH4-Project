39a8b3e3c717768a16d148e918d24da35d789b4d
mw08081 <president304@naver.com>
2025-12-26 10:20:15 +0900

    [Add] Waiting level && WaitingLvGameModeBase

---
+++ Source/CH4TopDownProject/Game/RCWaitingLvGameModeBase.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/RCWaitingLvGameModeBase.h"
+
+#include "Controller/RCPlayerController.h"
+#include "Game/RCGameInstance.h"
+#include "Game/RCGameStateBase.h"
+
+void ARCWaitingLvGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitGameLv();
+}
+
+void ARCWaitingLvGameModeBase::InitGameLv()
+{
+	CurGameStateChangeDelay = GAME_START_DELAY;
+
+	GetWorld()->GetTimerManager().SetTimer(MainTimerHandle, this, &ThisClass::OnMainTimerElapsed, 1.f, true);
+}
+
+void ARCWaitingLvGameModeBase::OnMainTimerElapsed()
+{
+	ARCGameStateBase* RCGameState = GetGameState<ARCGameStateBase>();
+	if (IsValid(RCGameState) == false)
+	{
+		return;
+	}
+
+	RCGameState->AlivePlayerControllerCount = LogInPlayerControllers.Num();
+
+	switch (RCGameState->MatchState)
+	{
+	case EMatchState::None: { break; }
+	case EMatchState::Waiting:
+	{
+		if (LogInPlayerControllers.Num() < MaxPlayerCount)
+		{
+			UE_LOG(LogTemp, Error, TEXT("session is waiting.."));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("%d seconds until StartGame"), CurGameStateChangeDelay);
+			--CurGameStateChangeDelay;
+		}
+
+		if (CurGameStateChangeDelay <= 0 && bIsLoadedToBattleLv == false)
+		{
+			UE_LOG(LogTemp, Error, TEXT("Server Travel"));
+			bIsLoadedToBattleLv = true;
+			GetWorld()->ServerTravel(TEXT("Battle_level"), true);
+		}
+		break;
+	}
+	case EMatchState::Playing: { break; }
+	case EMatchState::Ending: { break; }
+	case EMatchState::Cleaning: { break; }
+	case EMatchState::End: { break; }
+	default: { break; }
+	}
+
+}
+
+void ARCWaitingLvGameModeBase::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	ARCPlayerController* NewPlayerController = Cast<ARCPlayerController>(NewPlayer);
+	if (NewPlayerController != nullptr)
+	{
+		LogInPlayerControllers.Add(NewPlayerController);
+
+		if (LogInPlayerControllers.Num() >= MaxPlayerCount) {
+			UE_LOG(LogTemp, Error, TEXT("Player is full. This Session will be closed..."));
+
+			URCGameInstance* RCGameInstance = GetWorld()->GetGameInstance<URCGameInstance>();
+			if (IsValid(RCGameInstance))
+			{
+				RCGameInstance->ManageSession(true);
+			}
+		}
+	}
+}
+
+void ARCWaitingLvGameModeBase::Logout(AController* ExitingPlayer)
+{
+	Super::Logout(ExitingPlayer);
+
+	ARCPlayerController* ExitingRCPlayerController = Cast<ARCPlayerController>(ExitingPlayer);
+	if (IsValid(ExitingPlayer))
+	{
+		if (LogInPlayerControllers.Find(ExitingRCPlayerController) != INDEX_NONE)
+		{
+			LogInPlayerControllers.Remove(ExitingRCPlayerController);
+		}
+	}
+}
---
+++ Source/CH4TopDownProject/Game/RCWaitingLvGameModeBase.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RCWaitingLvGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH4TOPDOWNPROJECT_API ARCWaitingLvGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	void InitGameLv();
+
+#pragma region Settings
+
+protected:
+	const int32 GAME_START_DELAY = 10;
+	int32 CurGameStateChangeDelay = 10;
+	bool bIsLoadedToBattleLv = false;
+
+private:
+	UFUNCTION()
+	void OnMainTimerElapsed();
+
+public:
+	FTimerHandle MainTimerHandle;
+
+
+protected:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* ExitingPlayer) override;
+
+	const int32 MaxPlayerCount = 2;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TArray<TObjectPtr<class ARCPlayerController>> LogInPlayerControllers;
+};
