1109562ef9c4fcd54ae0ba6776c3ec1182a1b1a9
ko gyeong tae <rudex1598@gmail.com>
2025-12-26 14:34:52 +0900

    [Add] 상자의 상호작용 범위를 벗어나면 상자 ui가 자동으로 꺼지게 추가

--- Source/CH4TopDownProject/Interactor/Chest.h
+++ Source/CH4TopDownProject/Interactor/Chest.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -34,6 +34,10 @@ public:
 
 	virtual void Interact_Implementation(AActor* Interactor) override;
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void OnSphereEnd(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex)override;
 
 	void SetItem(const TMap<FName, int32>& ItemMap);
 
