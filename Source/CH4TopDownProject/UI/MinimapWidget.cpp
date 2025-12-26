fcc7cb9a04da985cfcfe738bd5b2a876c662b797
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-12-26 20:32:32 +0900

    [Add] 미니맵에 자기장 영역 표시

--- Source/CH4TopDownProject/UI/MinimapWidget.cpp
+++ Source/CH4TopDownProject/UI/MinimapWidget.cpp
@@ -1,20 +1,49 @@
 #include "UI/MinimapWidget.h"
 #include "Components/Image.h"
 #include "GameFramework/Pawn.h"
+#include "EngineUtils.h"
+#include "GameEvent/BlueZoneActor.h"
 
 void UMinimapWidget::NativeConstruct()
 {
     Super::NativeConstruct();
+
+    for (TActorIterator<ABlueZoneActor> It(GetWorld()); It; ++It)
+    {
+        BlueZoneActor = *It;
+        break;
+    }
+
+    if (BlueZoneMaskImage)
+    {
+        BlueZoneMID = BlueZoneMaskImage->GetDynamicMaterial();
+    }
 }
 
 void UMinimapWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
     Super::NativeTick(MyGeometry, InDeltaTime);
-    
-    //APawn* OwningPawn = GetOwningPlayerPawn();
-    //if (OwningPawn && PlayerIcon)
-    //{
-    //    float PlayerYaw = OwningPawn->GetActorRotation().Yaw;
-    //    PlayerIcon->SetRenderTransformAngle(PlayerYaw);
-    //}
+
+    if (!BlueZoneActor || !BlueZoneMID)
+    { 
+        return; 
+    }
+
+    APawn* OwningPawn = GetOwningPlayerPawn();
+    FVector CaptureCenter = OwningPawn ? OwningPawn->GetActorLocation() : FVector::ZeroVector;
+
+    const FVector BlueZoneWorldPos = BlueZoneActor->GetActorLocation();
+
+    float MapSize = 10000.0f;
+
+    float U = (BlueZoneWorldPos.Y - CaptureCenter.Y) / MapSize + 0.5f;
+    float V = (BlueZoneWorldPos.X - CaptureCenter.X) / MapSize + 0.5f;
+
+    V = 1.0f - V;
+
+    const FVector2D BlueZoneCenterUV(U, V);
+    const float BlueZoneRadiusUV = BlueZoneActor->GetCurrentRadius() / MapSize;
+
+    BlueZoneMID->SetVectorParameterValue(TEXT("BlueZoneCenterUV"), FVector(BlueZoneCenterUV, 0));
+    BlueZoneMID->SetScalarParameterValue(TEXT("BlueZoneRadiusUV"), BlueZoneRadiusUV);
 }
